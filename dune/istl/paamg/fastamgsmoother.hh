97fb6f929ee11f4c70407be9abccb4e578706728
Dominic Kempf <dominic.r.kempf@gmail.com>
2013-10-10 15:32:37 +0200

    [cleanup] [Doc] polishing the code

--- dune/istl/paamg/fastamgsmoother.hh
+++ dune/istl/paamg/fastamgsmoother.hh
@@ -18,7 +18,6 @@ namespace Dune
         typedef typename M::ConstRowIterator RowIterator;
         typedef typename M::ConstColIterator ColIterator;
         typedef typename Y::block_type YBlock;
-        typedef typename X::block_type XBlock;
 
         typename Y::iterator dIter=d.begin();
         typename Y::const_iterator bIter=b.begin();
@@ -27,59 +26,57 @@ namespace Dune
         for(RowIterator row=A.begin(), end=A.end(); row != end;
             ++row, ++dIter, ++xIter, ++bIter)
         {
-          ColIterator col=(*row).begin();
           *dIter = *bIter;
 
+          // do lower triangular matrix
+          ColIterator col=(*row).begin();
           for (; col.index()<row.index(); ++col)
-            (*col).mmv(x[col.index()],*dIter); // rhs -= sum_{j<i} a_ij * xnew_j
+            (*col).mmv(x[col.index()],*dIter); // d -= sum_{j<i} a_ij * xnew_j
 
-          assert(row.index()==col.index());
           ColIterator diag=col;
 
+          // do upper triangular matrix only if not the first iteration
+          // because x would be 0 anyway, store result in v for latter use
           YBlock v = 0;
-          // do upper triangle only if not the first iteration because x is 0 anyway
           if (!first)
           {
-            ColIterator colEnd = row->end();
             //skip diagonal and iterate over the rest
+            ColIterator colEnd = row->end();
             for (++col; col != colEnd; ++col)
-            {
-              (*col).mmv(x[col.index()],*dIter);
               (*col).umv(x[col.index()],v);
-            }
           }
+          *dIter -= v;
 
-          //either go on the next blocklevel recursively or just solve with diagonal (TMP)
+          // TODO either go on the next blocklevel recursively or just solve with diagonal (TMP)
           //GaussSeidelStepWithDefect<level-1>::forward_apply(*diag,*xIter,*dIter,*bIter,first,compDef);
           diag->solve(*xIter,*dIter);
 
+          // compute defect if necessary
           if (compDef)
           {
-            if (first)
-              *dIter=0;
-            else
-              *dIter = v;
-
+            *dIter = v;
 
             // Update residual for the symmetric case
+            // (uses A_ji=A_ij for memory access efficiency)
             for(col=(*row).begin(); col.index()<row.index(); ++col)
               col->mmv(*xIter, d[col.index()]);     //d_j-=A_ij x_i
           }
         }
-        if (compDef)
-        {
-          Y newdef(b);
-          typename Y::iterator dit = newdef.begin();
-          typename X::iterator xit = x.begin();
-          for (RowIterator row = A.begin(); row != A.end(); ++row, ++xit, ++dit)
-            for(ColIterator col = row->begin(); col!=row->end(); ++col)
-              col->mmv(x[col.index()], *dit);
-              //col->mmv(*xit, d[col.index()]);
-          for (int i=0; i<newdef.size(); i++)
-           // std::cout << newdef[i] << " " << d[i] << std::endl;
-            if (std::abs(newdef[i][0]-d[i][0])>1e-4)
-              DUNE_THROW(Dune::Exception,"Falschen Defekt berechnet");
-        }
+        // precious debug code
+//         if (compDef)
+//         {
+//           Y newdef(b);
+//           typename Y::iterator dit = newdef.begin();
+//           typename X::iterator xit = x.begin();
+//           for (RowIterator row = A.begin(); row != A.end(); ++row, ++xit, ++dit)
+//             for(ColIterator col = row->begin(); col!=row->end(); ++col)
+//               col->mmv(x[col.index()], *dit);
+//               //col->mmv(*xit, d[col.index()]);
+//           for (int i=0; i<newdef.size(); i++)
+//            // std::cout << newdef[i] << " " << d[i] << std::endl;
+//             if (std::abs(newdef[i][0]-d[i][0])>1e-4)
+//               DUNE_THROW(Dune::Exception,"Falschen Defekt berechnet");
+//         }
       }
 
       template<typename M, typename X, typename Y>
@@ -88,8 +85,6 @@ namespace Dune
       {
         typedef typename M::ConstRowIterator RowIterator;
         typedef typename M::ConstColIterator ColIterator;
-        typedef typename Y::block_type YBlock;
-        typedef typename X::block_type XBlock;
 
         typename Y::iterator dIter=d.beforeEnd();
         typename X::iterator xIter=x.beforeEnd();
@@ -98,28 +93,22 @@ namespace Dune
         for(RowIterator row=A.beforeEnd(), end=A.beforeBegin(); row != end;
             --row, --dIter, --xIter, --bIter)
         {
-          ColIterator endCol=(*row).beforeBegin();
-          ColIterator col=(*row).beforeEnd();
           *dIter = *bIter;
 
+          // do lower triangular matrix
+          ColIterator endCol=(*row).beforeBegin();
+          ColIterator col=(*row).beforeEnd();
           for (; col.index()>row.index(); --col)
-            (*col).mmv(x[col.index()],*dIter);     // rhs -= sum_{i>j} a_ij * xnew_j
-          assert(row.index()==col.index());
+            (*col).mmv(x[col.index()],*dIter);     // d -= sum_{i>j} a_ij * xnew_j
+
           ColIterator diag=col;
-          YBlock v=*dIter;
-          // upper diagonal matrix
+
+          // do upper triangular matrix manually skipping diagonal
           for (--col; col!=endCol; --col)
-            (*col).mmv(x[col.index()],v);     // v -= sum_{j<i} a_ij * xold_j
+            (*col).mmv(x[col.index()],*dIter);     // d -= sum_{j<i} a_ij * xold_j
 
-          // Not recursive yet. Just solve with the diagonal
-          diag->solve(*xIter,v);
-
-          *dIter-=v; //TODO wieso genau diese Zeile (braucht man das residual denn nun?)
-
-          // Update residual for the symmetric case
-          // Skip residual computation as it is not needed.
-//           for(col=(*row).begin();col.index()<row.index(); ++col)
-//             (*col).mmv(*xIter, d[col.index()]); //d_j-=A_ij x_i
+          // TODO Not recursive yet. Just solve with the diagonal
+          diag->solve(*xIter,*dIter);
         }
       }
     };
@@ -149,16 +138,15 @@ namespace Dune
       static void apply(const M& A, X& x, Y& d,
                         const Y& b, int num_iter)
       {
-        // perform iterations. These have to know whether they are first and whether to compute a defect.
-        // arguments are preferred over template paramters here to reduce compile time/program size
-        // In contrast the needed if-clauses are performance irrelevant.
+        // perform iterations. These have to know whether they are first
+        // and whether to compute a defect.
         if (num_iter == 1)
           GaussSeidelStepWithDefect<M::blocklevel>::forward_apply(A,x,d,b,true,true);
         else
         {
           GaussSeidelStepWithDefect<M::blocklevel>::forward_apply(A,x,d,b,true,false);
           for (int i=0; i<num_iter-2; i++)
-            GaussSeidelStepWithDefect<M::blocklevel>::forward_apply(A,x,d,b);
+            GaussSeidelStepWithDefect<M::blocklevel>::forward_apply(A,x,d,b, false,false);
           GaussSeidelStepWithDefect<M::blocklevel>::forward_apply(A,x,d,b,false,true);
         }
       }
