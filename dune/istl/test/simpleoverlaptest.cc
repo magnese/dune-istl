62819d4727a5ccc68508739bc6cd276b4353922c
Marco Agnese <marco.agnese88@gmail.com>
2015-05-18 01:10:14 +0100

    fixed cmake build and cleanup

--- dune/istl/test/simpleoverlaptest.cc
+++ dune/istl/test/simpleoverlaptest.cc
@@ -25,55 +25,38 @@
 #define BLOCK_DIM 1
 #define DEBUG_FLAG 1
 
-// parallel sync print of a value for all the processes
+// parallel sync print for all the processes
 template<typename T,typename C>
-void printAll(std::string&& str,T& value,C& comm){
-  #ifdef DEBUG_FLAG
-  #if DEBUG_FLAG
-  for(size_t i=0;i!=comm.size();++i){
-if(comm.rank()==i) std::cout<<"[rank "<<comm.rank()<<"] "<<str<<value<<std::endl;
-    comm.barrier();
-  }
-  #endif
-  #endif
+inline void printAll(const std::string& str,const T& value,const C& comm)
+{
+  for(int i=0;i!=comm.size();++i)
+    printOne(str,value,comm,i);
 }
 
 // parallel sync print of a value for only 1 process (default rank=0)
 template<typename T,typename C>
-void printOne(std::string&& str,T& value,C& comm,size_t rank=0){
+inline void printOne(const std::string& str,const T& value,const C& comm,const int& rank=0)
+{
   #ifdef DEBUG_FLAG
   #if DEBUG_FLAG
-  if(comm.rank()==rank) std::cout<<str<<value<<std::endl;
+  if(comm.rank()==rank)
+    std::cout<<"[rank "<<rank<<"] "<<str<<value<<std::endl;
   comm.barrier();
-  #endif
-  #endif
-}
-
-// parallel sync print of a vector for all the processes
-template<typename T,typename C>
-void printAll(std::string&& str,std::vector<T>& v,C& comm){
-  #ifdef DEBUG_FLAG
-  #if DEBUG_FLAG
-  for(size_t i=0;i!=comm.size();++i){
-    if(comm.rank()==i){
-      std::cout<<"[rank "<<comm.rank()<<"] "<<str;
-      for(typename std::vector<T>::iterator it=v.begin();it!=v.end();++it) std::cout<<*it<<" ";
-      std::cout<<std::endl;
-    }
-    comm.barrier();
-  }
   #endif
   #endif
 }
 
 // parallel sync print of a vector for only 1 process (default rank=0)
 template<typename T,typename C>
-void printOne(std::string&& str,std::vector<T>& v,C& comm,size_t rank=0){
+void printOne(const std::string& str,const std::vector<T>& v,const C& comm,const int& rank=0)
+{
   #ifdef DEBUG_FLAG
   #if DEBUG_FLAG
-  if(comm.rank()==rank){
-    std::cout<<str;
-    for(typename std::vector<T>::iterator it=v.begin();it!=v.end();++it) std::cout<<*it<<" ";
+  if(comm.rank()==rank)
+  {
+    std::cout<<"[rank "<<rank<<"] "<<str;
+    for(auto val:v)
+      std::cout<<val<<" ";
     std::cout<<std::endl;
   }
   comm.barrier();
@@ -82,18 +65,25 @@ void printOne(std::string&& str,std::vector<T>& v,C& comm,size_t rank=0){
 }
 
 template<typename M,typename C>
-void printBMatrix(std::string&& str,M& matrix,C& comm){
+void printBMatrix(const std::string& str,const M& matrix,const C& comm)
+{
   #ifdef DEBUG_FLAG
   #if DEBUG_FLAG
-  typename M::block_type nullmatrix(0.0);
-  for(size_t i=0;i!=comm.size();++i){
-    if(comm.rank()==i){
-      std::cout<<"[rank "<<comm.rank()<<"] "<<str;
-      for(size_t row=0;row!=matrix.N();++row){
+  typename M::block_type nullblock(0.0);
+  for(int i=0;i!=comm.size();++i)
+  {
+    if(comm.rank()==i)
+    {
+      std::cout<<"[rank "<<i<<"] "<<str;
+      for(std::size_t row=0;row!=matrix.N();++row)
+      {
         std::cout<<"row "<<row<<":"<<std::endl;
-        for(size_t col=0;col!=matrix.M();++col){
-          if(matrix.exists(row,col)) std::cout<<matrix[row][col];
-          else std::cout<<nullmatrix;
+        for(std::size_t col=0;col!=matrix.M();++col)
+        {
+          if(matrix.exists(row,col))
+            std::cout<<matrix[row][col];
+          else
+            std::cout<<nullblock;
           std::cout<<std::endl;
         }
       }
@@ -104,134 +94,124 @@ void printBMatrix(std::string&& str,M& matrix,C& comm){
   #endif
 }
 
-int main(int argc,char** argv){
-
+int main(int argc,char** argv)
+{
   // init MPI
   MPI_Init(&argc,&argv);
   Dune::CollectiveCommunication<MPI_Comm> commColl(MPI_COMM_WORLD);
 
   // get size and rank
-  const size_t size(commColl.size());
-  const size_t rank(commColl.rank());
+  const int size(commColl.size());
+  const int rank(commColl.rank());
 
   // check if the code is run on 2 processes
-  if(size!=2){
+  if(size!=2)
     printOne("Error: run the code again with 2 processes!","",commColl);
-    MPI_Finalize();
-    return 1;
+  else
+  {
+    // dimmension
+    const std::size_t blockDim(BLOCK_DIM);
+    const std::size_t dim(rank+2);
+
+    // create vector and matrix
+    typedef int FieldType;
+
+    typedef Dune::FieldMatrix<FieldType,blockDim,blockDim> MatrixBlockType;
+    typedef Dune::BCRSMatrix<MatrixBlockType> MatrixType;
+    MatrixType A(dim,dim,MatrixType::random);
+
+    typedef Dune::FieldVector<FieldType,blockDim> VectorBlockType;
+    typedef Dune::BlockVector<VectorBlockType> VectorType;
+    VectorType x(dim);
+    VectorType y(dim);
+
+    // fill vector x
+    if(rank==0)
+      x[0]=3;
+    else
+    {
+      x[0]=2;
+      x[1]=1;
+    }
+    printAll("x before communication\n",x,commColl);
+
+    // fill matrix A
+    if(rank==0)
+    {
+      A.setrowsize(0,2);
+      A.setrowsize(1,0);
+      A.endrowsizes();
+      A.addindex(0,0);
+      A.addindex(0,1);
+      A.endindices();
+      A[0][0]=1;
+      A[0][1]=2;
+    }
+    else
+    {
+      A.setrowsize(0,2);
+      A.setrowsize(1,1);
+      A.setrowsize(2,2);
+      A.endrowsizes();
+      A.addindex(0,1);
+      A.addindex(0,2);
+      A.addindex(1,0);
+      A.addindex(2,1);
+      A.addindex(2,2);
+      A.endindices();
+      A[0][1]=3;
+      A[0][2]=2;
+      A[1][0]=1;
+      A[2][1]=2;
+      A[2][2]=1;
+    }
+    printBMatrix("A\n",A,commColl);
+
+    // setup communication
+    typedef Dune::OwnerOverlapCopyCommunication<size_t> OverlapCommunicationType;
+    OverlapCommunicationType commOverlap(MPI_COMM_WORLD);
+
+    typedef Dune::OwnerOverlapCopyAttributeSet AttributeSetType;
+    typedef AttributeSetType::AttributeSet Flag;
+    typedef Dune::ParallelLocalIndex<Flag> LocalIndexType;
+
+    typedef OverlapCommunicationType::ParallelIndexSet ParallelIndexSetType;
+    ParallelIndexSetType& indices(commOverlap.indexSet());
+
+    indices.beginResize();
+    if(rank==0)
+    {
+      indices.add(0,LocalIndexType(0,AttributeSetType::owner,true));
+      indices.add(2,LocalIndexType(1,AttributeSetType::copy,true));
+    }
+    else
+    {
+      indices.add(1,LocalIndexType(0,AttributeSetType::owner,true));
+      indices.add(2,LocalIndexType(1,AttributeSetType::owner,true));
+      indices.add(0,LocalIndexType(2,AttributeSetType::copy,true));
+    }
+    indices.endResize();
+    printAll("Parallel index set\n",indices,commColl);
+
+    // remote indices already built
+    commOverlap.remoteIndices().rebuild<false>();
+
+    // fill x with the values which need to be copied
+    commOverlap.copyOwnerToAll(x,x);
+    printAll("x after communication\n",x,commColl);
+
+    // perform the product y=Ax
+    Dune::OverlappingSchwarzOperator<MatrixType,VectorType,VectorType,OverlapCommunicationType> schwarzOperator(A,commOverlap);
+    schwarzOperator.apply(x,y);
+    printAll("y=Ax using Schwarz operator\n",y,commColl);
+
+    // fill y with the values which need to be copied
+    commOverlap.copyOwnerToAll(y,y);
+    printAll("y after communication\n",y,commColl);
   }
-
-  // dimmension
-  const size_t blockDim(BLOCK_DIM);
-  const size_t dim(rank+2);
-
-  // create vector and matrix
-  typedef int FieldType;
-
-  typedef Dune::FieldMatrix<FieldType,blockDim,blockDim> MatrixBlockType;
-  typedef Dune::BCRSMatrix<MatrixBlockType> MatrixType;
-  MatrixType A(dim,dim,MatrixType::random);
-
-  typedef Dune::FieldVector<FieldType,blockDim> VectorBlockType;
-  typedef Dune::BlockVector<VectorBlockType> VectorType;
-  VectorType x(dim);
-  VectorType y(dim);
-
-  // fill vector x
-  if(rank==0){
-    x[0]=3;
-  }
-  else{
-    x[0]=2;
-    x[1]=1;
-  }
-
-  printAll("x before communication\n",x,commColl);
-  printOne("","",commColl);
-
-  // fill matrix A
-  if(rank==0){
-    A.setrowsize(0,2);
-    A.setrowsize(1,0);
-    A.endrowsizes();
-    A.addindex(0,0);
-    A.addindex(0,1);
-    A.endindices();
-    A[0][0]=1;
-    A[0][1]=2;
-  }
-  else{
-    A.setrowsize(0,2);
-    A.setrowsize(1,1);
-    A.setrowsize(2,2);
-    A.endrowsizes();
-    A.addindex(0,1);
-    A.addindex(0,2);
-    A.addindex(1,0);
-    A.addindex(2,1);
-    A.addindex(2,2);
-    A.endindices();
-    A[0][1]=3;
-    A[0][2]=2;
-    A[1][0]=1;
-    A[2][1]=2;
-    A[2][2]=1;
-  }
-
-  printBMatrix("A\n",A,commColl);
-  printOne("","",commColl);
-
-  // setup communication
-  typedef Dune::OwnerOverlapCopyCommunication<size_t> OverlapCommunicationType;
-  OverlapCommunicationType commOverlap(MPI_COMM_WORLD);
-
-  typedef Dune::OwnerOverlapCopyAttributeSet AttributeSetType;
-  typedef AttributeSetType::AttributeSet Flag;
-  typedef Dune::ParallelLocalIndex<Flag> LocalIndexType;
-
-  typedef OverlapCommunicationType::ParallelIndexSet ParallelIndexSetType;
-  ParallelIndexSetType& indices(commOverlap.indexSet());
-
-  indices.beginResize();
-  if(rank==0){
-    indices.add(0,LocalIndexType(0,AttributeSetType::owner,true));
-    indices.add(2,LocalIndexType(1,AttributeSetType::copy,true));
-  }
-  else{
-    indices.add(1,LocalIndexType(0,AttributeSetType::owner,true));
-    indices.add(2,LocalIndexType(1,AttributeSetType::owner,true));
-    indices.add(0,LocalIndexType(2,AttributeSetType::copy,true));
-  }
-  indices.endResize();
-
-  printAll("Parallel index set\n",indices,commColl);
-  printOne("","",commColl);
-
-  // remote indices already built
-  commOverlap.remoteIndices().rebuild<false>();
-
-  // fill x with the values which need to be copied
-  commOverlap.copyOwnerToAll(x,x);
-
-  printAll("x after communication\n",x,commColl);
-  printOne("","",commColl);
-
-  // perform the product y=Ax
-  Dune::OverlappingSchwarzOperator<MatrixType,VectorType,VectorType,OverlapCommunicationType> schwarzOperator(A,commOverlap);
-  schwarzOperator.apply(x,y);
-
-  printAll("y=Ax using Schwarz operator\n",y,commColl);
-  printOne("","",commColl);
-
-  // fill y with the values which need to be copied
-  commOverlap.copyOwnerToAll(y,y);
-
-  printAll("y after communication\n",y,commColl);
-  printOne("","",commColl);
 
   // finalize MPI
   MPI_Finalize();
 
   return 0;
-
 }
